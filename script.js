9332905c82078b70ccc7151401d934ff6e213e32
Robert Christian <robert.44azz@gmail.com>
2024-06-26 17:29:47 -0400

    v1.1

---
+++ script.js
@@ -0,0 +1,14 @@
+function showMenu() {
+  let menuMobile = document.querySelector(".mobile-menu-list")
+  if (menuMobile.classList.contains("open")) {
+    menuMobile.classList.remove("open")
+  } else {
+    menuMobile.classList.add("open")
+  }
+
+}
+
+
+
+
+
